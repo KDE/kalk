3b1f352c657c92b3a42333e09be9f63e146d48ad
Han Young <hanyoung@protonmail.com>
2021-01-04 22:03:03 +0800

    add InputManager to manager input logic

---
+++ src/inputmanager.cpp
@@ -0,0 +1,89 @@
+/*
+ * SPDX-FileCopyrightText: 2020-2021 Han Young <hanyoung@protonmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#include "inputmanager.h"
+#include "mathengine.h"
+#include "historymanager.h"
+InputManager::InputManager()
+{
+}
+
+InputManager *InputManager::inst()
+{
+    static InputManager singleton;
+    return &singleton;
+}
+
+const QString &InputManager::expression() const
+{
+    return m_expression;
+}
+void InputManager::setExpression(const QString &expression)
+{
+    m_expression = expression;
+}
+
+const QString &InputManager::result() const
+{
+    return m_result;
+}
+
+void InputManager::append(const QString &subexpression)
+{
+    MathEngine::inst()->parse(m_expression + subexpression);
+    if(!MathEngine::inst()->error())
+    {
+        m_stack.push(subexpression.size());
+        m_result = MathEngine::inst()->result();
+        m_expression += subexpression;
+        Q_EMIT resultChanged();
+        Q_EMIT expressionChanged();
+    }
+}
+
+void InputManager::backspace()
+{
+    if(m_stack.size())
+    {
+        m_expression.chop(m_stack.top());
+        Q_EMIT expressionChanged();
+        MathEngine::inst()->parse(m_expression);
+        if(!MathEngine::inst()->error())
+        {
+            m_result = MathEngine::inst()->result();
+            Q_EMIT resultChanged();
+        }
+    }
+}
+
+void InputManager::equal()
+{
+    HistoryManager::inst()->addHistory(m_expression);
+    m_expression = m_result;
+    m_result.clear();
+    m_stack = {}; // clear the stack
+    m_stack.push(m_result.size());
+    Q_EMIT expressionChanged();
+    Q_EMIT resultChanged();
+}
+
+void InputManager::clear()
+{
+    m_expression.clear();
+    m_result.clear();
+    m_stack = {};
+    Q_EMIT expressionChanged();
+    Q_EMIT resultChanged();
+}
+
+void InputManager::fromHistory(const QString &result)
+{
+    m_expression = result;
+    m_result.clear();
+    m_stack = {};
+    m_stack.push(result.size());
+    Q_EMIT expressionChanged();
+    Q_EMIT resultChanged();
+}
---
+++ src/inputmanager.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2020-2021 Han Young <hanyoung@protonmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#pragma once
+#include <stack>
+#include <QObject>
+
+class InputManager : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString expression READ expression WRITE setExpression NOTIFY expressionChanged)
+    Q_PROPERTY(QString result READ result NOTIFY resultChanged)
+public:
+    static InputManager *inst();
+    const QString &expression() const;
+    void setExpression(const QString &expression);
+    const QString &result() const;
+    Q_INVOKABLE void append(const QString &subexpression);
+    Q_INVOKABLE void backspace();
+    Q_INVOKABLE void equal();
+    Q_INVOKABLE void clear();
+    Q_INVOKABLE void fromHistory(const QString &result);
+Q_SIGNALS:
+    void expressionChanged();
+    void resultChanged();
+private:
+    InputManager();
+    std::stack<int, std::vector<int>> m_stack; // track subexpression length for removal later
+    QString m_expression;
+    QString m_result;
+};
+
